c58bfc6da93a9da95c4258fe69a4ed04c4eabdf5
Faraz Fallahi <fffaraz@gmail.com>
2026-07-07 17:51:17 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
+VERSION=$(git describe --tags --always --dirty 2>/dev/null || echo dev)
+
 rm -f dupdig
-CGO_ENABLED=0 go build -ldflags='-s -w' -trimpath -o dupdig .
+CGO_ENABLED=0 go build -ldflags="-s -w -X main.version=${VERSION}" -trimpath -o dupdig .
